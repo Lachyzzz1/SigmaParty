1f98d0f0cf6632f7e4ffeb91f440a1d782fe3e52
FinnR <rimmerfinn@gmail.com>
2024-06-22 09:51:04 +1200

    more scraper functionality

--- NZXAPI/tools/WebScraper.java
+++ NZXAPI/tools/WebScraper.java
@@ -10,23 +10,23 @@ import java.util.regex.Pattern;
 
 public class WebScraper {
 
-    public static void main(String[] args) {
-        String[] stockCodes = {"FNZ", "ASD", "ASP", "BGP", "BRM", "CCC", "CEN", "CMO", "BLT", "ATM","NZG","NZO","MMH"};
-        for(int x = 0; x < stockCodes.length; x++){
-            System.out.println(getStockPrice(stockCodes[x]));
-        }
-        
+
+    private String url;
+    private String htmlContents;
+
+    public WebScraper(String url) {
+        this.url = url;
+        this.htmlContents = getHTMLContents(url); 
     }
 
-    public static BigDecimal getStockPrice(String stockCode) {
-        String urlString = "https://www.nzx.com/instruments/" + stockCode;
-        BigDecimal result = null;
+    public static String getHTMLContents(String url){
+        String result = null;
         try {
             // Create a URL object from the URL string
-            URL url = new URL(urlString);
+            URL urlObject = new URL(url);
 
             // Open a connection to the URL
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            HttpURLConnection connection = (HttpURLConnection) urlObject.openConnection();
 
             // Set the request method to GET
             connection.setRequestMethod("GET");
@@ -45,35 +45,78 @@ public class WebScraper {
                     response.append(inputLine);
                 }
                 in.close();
-
-                // Print the response
-                //System.out.println("Response: " + response.toString());
-
-                // Define the regex pattern to extract the stock price
-                String regex = "<h3 class=\"InstrumentPriceAmount cMIYIY p-price\">\\$(.*?)</h3>";
-
-                // Use Pattern and Matcher to find the stock price
-                Pattern pattern = Pattern.compile(regex);
-                Matcher matcher = pattern.matcher(response.toString());
                 
-                // Check if the pattern is found and extract the price
-                if (matcher.find()) {
-                    String stockPrice = matcher.group(1).substring(0);
-                    result = new BigDecimal(stockPrice);
-                    System.out.println("Close Stock Price: $" + stockPrice);
-                } else {
-                    System.out.println("Stock price not found.");
-                }
-
+                result = response.toString();
             } else {
                 System.out.println("GET request failed. Response code: " + responseCode);
             }
 
         } catch (Exception e) {
             e.printStackTrace();
+    }
+    return result;
+    }
+
+    public String getHTMLContents(){
+        return this.htmlContents;
+    }
+
+    public BigDecimal getStockPrice() {
+       
+         // Define the regex pattern to extract the stock price
+        String regex = "<h3 class=\"InstrumentPriceAmount cMIYIY p-price\">\\$(.*?)</h3>";
+        String changePattern = "<span class=\"PriceChangeAmount emeBBK p-price-change kGHgXH\">(.*?)</span>";
+
+                // Use Pattern and Matcher to find the stock price
+        Pattern pattern = Pattern.compile(regex);
+        Pattern changeRegex = Pattern.compile(changePattern);
+
+        Matcher matcher = pattern.matcher(this.htmlContents);
+        Matcher changeMatcher = changeRegex.matcher(this.htmlContents);
+        BigDecimal result = null;
+                
+                // Check if the pattern is found and extract the price
+        if (matcher.find()) {
+            String stockPrice = matcher.group(1).substring(0);
+            result = new BigDecimal(stockPrice);
+            System.out.println("Close Stock Price: $" + stockPrice);
+        } else {
+            System.out.println("Stock price not found.");
         }
+        if (changeMatcher.find()) {
+            System.out.println("Price Change: " + changeMatcher.group(1));
+        } else {
+            System.out.println("Price change not found");
+        }
+
         return result;
     }
-    }
+    
+
+
+    public BigDecimal getPriceChange() {
+       
+        // Define the regex pattern to extract the stock price
+       String changePattern = "<span class=\"PriceChangeAmount emeBBK p-price-change kGHgXH\">(.*?)</span>";
+
+               // Use Pattern and Matcher to find the stock price
+       Pattern changeRegex = Pattern.compile(changePattern);
+
+       Matcher changeMatcher = changeRegex.matcher(this.htmlContents);
+       BigDecimal result = null;
+               
+               // Check if the pattern is found and extract the price
+      
+       if (changeMatcher.find()) {
+        String priceChange = changeMatcher.group(1).replace("$","");   
+        System.out.println("Price Change: " + changeMatcher.group(1));
+        result = new BigDecimal(priceChange);
+       } else {
+           System.out.println("Price change not found");
+       }
+
+       return result;
+   }
+   }
 
 
